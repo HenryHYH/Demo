0149f1ed99d641997ce91bc951f0b591d8942759
Henry <henry.hyh@gmail.com>
2014-12-17 18:06:31 +0800

    Simple.Data Hello world

---
+++ Simple.Data/Data.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace Data.Test
+{
+    public class CustomerRepositoryTest
+    {
+        private readonly CustomerRepository repository;
+
+        public CustomerRepositoryTest()
+        {
+            repository = new CustomerRepository();
+        }
+
+        [Theory]
+        [InlineData(1, "admin@nop.com", "admin@nop.com")]
+        [InlineData(2006, "Henry", "henry@abc.com")]
+        public void Test(int id, string expectedUsername, string expectedEmail)
+        {
+            var customer = repository.Get(id);
+            customer.ShouldNotBeNull();
+            customer.Email.ShouldBe(expectedEmail);
+            customer.Username.ShouldBe(expectedUsername);
+        }
+    }
+}
---
+++ Simple.Data/Data/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple.Data;
+using Model;
+
+namespace Data
+{
+    public class CustomerRepository
+    {
+        public Customer Get(int id)
+        {
+            return (Customer)Database.Default.Customer.Get(id);
+        }
+    }
+}
