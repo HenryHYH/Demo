71bca9669284f694a09cf689b53322e017259a6e
Henry <henry.hyh@gmail.com>
2014-12-18 13:58:27 +0800

    Topshelf Hello world

---
+++ Topshelf/Helloworld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<TimerService>(s =>
+                {
+                    s.ConstructUsing(name => new TimerService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalService();
+                x.SetServiceName("测试服务");
+                x.SetDisplayName("测试服务");
+                x.SetDescription("测试服务");
+            });
+        }
+    }
+}
---
+++ Topshelf/Helloworld/TimerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Helloworld
+{
+    public class TimerService
+    {
+        private readonly Timer timer;
+
+        public TimerService()
+        {
+            timer = new Timer(1000) { AutoReset = true };
+            timer.Elapsed += (sender, e) =>
+            {
+                Debug.WriteLine("Time is {0}.", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss", System.Globalization.DateTimeFormatInfo.CurrentInfo));
+                Console.WriteLine("Time is {0}.", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss", System.Globalization.DateTimeFormatInfo.CurrentInfo));
+            };
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
