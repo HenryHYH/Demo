68de0280ac616881dbf39021ab4ddb2161a29b1a
HenryHYH <henry.hyh@gmail.com>
2015-01-02 16:44:32 +0800

    添加 Admin Area

---
+++ FW/Presentations/FW.Web/Area/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FW.Service.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FW.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Fields
+
+        private readonly IUserService userService;
+
+        #endregion
+
+        #region Ctor
+
+        public HomeController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        #endregion
+
+        public ActionResult Index()
+        {
+            return Content("Admin index");
+        }
+
+        public ActionResult Test2()
+        {
+            return Json(userService.GetUsers(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
