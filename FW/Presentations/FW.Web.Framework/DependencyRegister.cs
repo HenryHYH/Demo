64c62b19450cca585fab77a6a126af5cd0cecc22
HenryHYH <henry.hyh@gmail.com>
2015-01-03 15:19:35 +0800

    调整引用

---
+++ FW/Presentations/FW.Web.Framework/DependencyRegister.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using FW.Core.Data;
+using FW.Core.Infrastructure;
+using FW.Data;
+using FW.Service.Users;
+using System.Linq;
+
+namespace FW.Web.Framework
+{
+    public class DependencyRegister : IDependencyRegister
+    {
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder)
+        {
+            //controllers
+            builder.RegisterControllers(typeFinder.GetAssemblies().ToArray());
+
+            // Settings
+            builder.Register(x => new SettingsManager().LoadSettings()).As<Settings>();
+
+            // Repository
+            builder.RegisterGeneric(typeof(MongoRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+
+            // Services
+            builder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+        }
+
+        public int Order
+        {
+            get { return 0; }
+        }
+    }
+}
