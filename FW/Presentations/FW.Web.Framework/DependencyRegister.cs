a71ca18921d062cbf8390674568744871012159d
HenryHYH <henry.hyh@gmail.com>
2015-05-12 09:20:22 +0800

    添加SqlServer EF支持

--- FW/Presentations/FW.Web.Framework/DependencyRegister.cs
+++ FW/Presentations/FW.Web.Framework/DependencyRegister.cs
@@ -39,6 +39,25 @@
             builder.Register(x => new SettingsManager().LoadSettings()).As<Settings>();
             builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
 
+            //data layer
+            var dataSettingsManager = new DataSettingsManager();
+            var dataProviderSettings = dataSettingsManager.LoadSettings();
+            builder.Register(c => dataSettingsManager.LoadSettings()).As<DataSettings>();
+            builder.Register(x => new EfDataProviderManager(x.Resolve<DataSettings>())).As<BaseDataProviderManager>().InstancePerDependency();
+            builder.Register(x => x.Resolve<BaseDataProviderManager>().LoadDataProvider()).As<IDataProvider>().InstancePerDependency();
+            if (dataProviderSettings != null && dataProviderSettings.IsValid())
+            {
+                var efDataProviderManager = new EfDataProviderManager(dataSettingsManager.LoadSettings());
+                var dataProvider = efDataProviderManager.LoadDataProvider();
+                dataProvider.InitConnectionFactory();
+
+                builder.Register<IDbContext>(c => new NopObjectContext(dataProviderSettings.DataConnectionString)).InstancePerRequest();
+            }
+            else
+            {
+                builder.Register<IDbContext>(c => new NopObjectContext(dataSettingsManager.LoadSettings().DataConnectionString)).InstancePerRequest();
+            }
+
             // Repository
             //builder.RegisterGeneric(typeof(MongoRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
             builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
