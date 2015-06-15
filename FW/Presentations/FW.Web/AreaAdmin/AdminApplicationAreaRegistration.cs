89d1b503aea58ffd3a3a9a448b1afe7c267f1313
HenryHYH <henry.hyh@gmail.com>
2015-06-15 22:57:02 +0800

    添加新 Area 修复无法 Update 的问题

---
+++ FW/Presentations/FW.Web/AreaAdmin/AdminApplicationAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FW.Admin
+{
+    public class AdminApplicationAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "AreaAdmin"; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_Default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", area = "AreaAdmin", id = UrlParameter.Optional },
+                new[] { "FW.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ FW/Presentations/FW.Web/AreaAdmin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FW.Service.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FW.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Fields
+
+        private readonly ILocalizationService localizationService;
+
+        #endregion
+
+        #region Ctor
+
+        public HomeController(ILocalizationService localizationService)
+        {
+            this.localizationService = localizationService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public ActionResult Index()
+        {
+            var resource = localizationService.GetResource(4);
+            resource.ResourceValue = "DEF";
+            localizationService.UpdateResource(resource);
+
+            return View();
+        }
+
+        #endregion
+    }
+}
