d0ad259d43410da332c417f8d4ade487790bef45
Henry <henry.hyh@gmail.com>
2015-06-20 21:49:38 +0800

    panel内固定表头

---
+++ FW/Presentations/FW.Web/AreaAdmin/Scripts/table-fixed-header.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    $(".table").fixedHeader();
+});
+
+; (function ($) {
+    $.fn.fixedHeader = function (options) {
+        var defaultOptions = {
+        };
+        if (options) $.extend(defaultOptions, options);
+
+        return this.each(function (i) {
+            var cur = $(this); // 当前table
+            var parent = cur.parent(); // 父级容器
+            var panelHeading = parent.siblings(".panel-heading");
+
+            // Panel标题的高度
+            var panelHeadingHeight = panelHeading.height()
+                            + parseFloat(panelHeading.css("padding-top"))
+                            + parseFloat(panelHeading.css("padding-bottom"))
+                            + parseFloat(panelHeading.css("border-top"))
+                            + parseFloat(panelHeading.css("border-bottom"))
+                            + 1;
+
+            // 表头容器
+            var fixedHeaderTable = cur.clone()
+                                        .addClass("table-fixed-header")
+                                        .attr({ id: "table-fixed-header-" + i })
+                                        .width(cur.width() + 2)
+                                        .css({ top: panelHeadingHeight })
+                                        .find("tbody").detach()
+                                        .end().prependTo(parent);
+
+            var scrollMin = cur.height() * -1;
+
+            parent.scroll(function () {
+                var scrollTop = cur.position().top - panelHeadingHeight;
+
+                if (scrollMin < scrollTop && scrollTop < 0)
+                    fixedHeaderTable.show();
+                else
+                    fixedHeaderTable.hide();
+            });
+
+        });
+    };
+})(jQuery);
