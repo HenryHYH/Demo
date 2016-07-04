e3cbb22cfc929d39cb082427d978661efbe8fa03
HenryHYH <henry.hyh@gmail.com>
2016-07-04 13:24:56 +0800

    添加 Dapper.FluentMap

---
+++ DapperDemo/ConsoleApp/Mapping/UserMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Model;
+using Dapper.FluentMap.Mapping;
+
+namespace ConsoleApp.Mapping
+{
+    public class UserMap : EntityMap<User>
+    {
+        public UserMap()
+        {
+            this.Map(x => x.Id).ToColumn("UId");
+            this.Map(x => x.Name).ToColumn("UName");
+        }
+    }
+}
---
+++ DapperDemo/ConsoleApp/TestFluentMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Mapping;
+using Dapper.FluentMap;
+using Xunit;
+using Dapper;
+using ConsoleApp.Model;
+
+namespace ConsoleApp
+{
+    public class TestFluentMap : BaseTest
+    {
+        public TestFluentMap()
+        {
+            FluentMapper.Initialize(x => x.AddMap(new UserMap()));
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var users = conn.Query<User>("SELECT UId, UName FROM [User] WITH(NOLOCK) ORDER BY UId");
+
+            Assert.Equal(2, users.Count());
+            Assert.Equal(1, users.FirstOrDefault().Id);
+        }
+    }
+}
