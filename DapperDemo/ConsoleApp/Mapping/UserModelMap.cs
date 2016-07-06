bc5d040e05f45f29e8d1368902011385b58eb790
HenryHYH <henry.hyh@gmail.com>
2016-07-06 10:38:00 +0800

    添加 FluentMap.Dommel

---
+++ DapperDemo/ConsoleApp/Mapping/UserModelMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Model;
+using Dapper.FluentMap.Dommel.Mapping;
+
+namespace ConsoleApp.Mapping
+{
+    public class UserModelMap : DommelEntityMap<UserModel>
+    {
+        public UserModelMap()
+        {
+            ToTable("User");
+            Map(x => x.Id).ToColumn("UId");
+            Map(x => x.Name).ToColumn("UName");
+        }
+    }
+}
---
+++ DapperDemo/ConsoleApp/TestDommelFluentMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Mapping;
+using ConsoleApp.Model;
+using Dapper;
+using Dapper.FluentMap;
+using Dapper.FluentMap.Dommel;
+using Dapper.FluentMap.Dommel.Mapping;
+using Xunit;
+
+namespace ConsoleApp
+{
+    public class TestDommelFluentMap : BaseTest
+    {
+        public TestDommelFluentMap()
+        {
+            FluentMapper.Initialize(x =>
+            {
+                x.AddMap(new UserModelMap());
+                x.ForDommel();
+            });
+        }
+
+        [Fact]
+        public void Test()
+        {
+            DommelEntityMap<UserModel> map = FluentMapper.EntityMaps[typeof(UserModel)] as DommelEntityMap<UserModel>;
+
+            var users = conn.Query<UserModel>("SELECT UId, UName FROM [User] WITH(NOLOCK) ORDER BY UId");
+
+            Assert.Equal(2, users.Count());
+            Assert.Equal(1, users.FirstOrDefault().Id);
+        }
+    }
+}
