a73e0ecd797506f7f7145aac38b9451f210ff2ef
HenryHYH <henry.hyh@gmail.com>
2016-07-11 09:03:04 +0800

    Init redis extension demo

---
+++ RedisExtensionDemo/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISerializer serializer = new NewtonsoftSerializer();
+            var c = ConnectionMultiplexer.Connect("localhost");
+            ICacheClient client = new StackExchangeRedisCacheClient(c, serializer);
+            client.Add("Test", new User()
+            {
+                Name = "Test",
+                Age = 100,
+                Birthday = DateTime.Now
+            });
+
+            Console.WriteLine(client.Get<User>("Test").ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}_{1}_{2:yyyy-MM-dd HH:mm:ss}", Name, Age, Birthday);
+        }
+    }
+}
