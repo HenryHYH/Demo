d461b7c522ead5fadc726e0e9bdd0a64c8c0f10c
HenryHYH <henry.hyh@gmail.com>
2016-07-14 16:15:18 +0800

    Finish RabbitMq Routing demo

---
+++ RabbitMqDemo/Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace EmitLogDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+
+                var input = Console.ReadLine();
+                while (!string.IsNullOrWhiteSpace(input))
+                {
+                    var severity = input.Split(' ')[0];
+                    var message = input.Split(' ')[1];
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "direct_logs",
+                        routingKey: severity,
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+                    input = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMqDemo/Routing/ReceiveLogsDirect/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsDirect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                var severities = args.Any() ? args : new[] { "info", "warning", "error" };
+                foreach (var severity in severities)
+                {
+                    channel.QueueBind(queue: queueName,
+                        exchange: "direct_logs",
+                        routingKey: severity);
+                }
+
+                Console.WriteLine(" [x] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+                };
+                channel.BasicConsume(queue: queueName,
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
