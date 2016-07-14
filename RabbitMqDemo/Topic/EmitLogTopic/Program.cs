831bfd7979f7be36d5d4c5673da57007e5bdd44c
HenryHYH <henry.hyh@gmail.com>
2016-07-14 16:50:40 +0800

    Finish RabbitMq Topic demo

---
+++ RabbitMqDemo/Topic/EmitLogTopic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace EmitLogTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+
+                var input = Console.ReadLine();
+                while (!string.IsNullOrWhiteSpace(input))
+                {
+                    var routingKey = input.Split(' ')[0];
+                    var message = input.Split(' ')[1];
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "topic_logs",
+                        routingKey: routingKey,
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                    input = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMqDemo/Topic/ReceiveLogsTopic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                var bindingKeys = args.Any() ? args : new[] { "kern.*", "*.critical" };
+                foreach (var bindingKey in bindingKeys)
+                {
+                    channel.QueueBind(queue: queueName,
+                        exchange: "topic_logs",
+                        routingKey: bindingKey);
+                }
+                Console.WriteLine(" [x] Waiting for message.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+                };
+                channel.BasicConsume(queue: queueName,
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
