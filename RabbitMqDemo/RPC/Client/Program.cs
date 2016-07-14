993d327e6281bc306be04ca421278e83fe2da2a8
HenryHYH <henry.hyh@gmail.com>
2016-07-14 17:34:08 +0800

    Init RabbitMq RPC demo

---
+++ RabbitMqDemo/RPC/Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RPCClient())
+            {
+                Console.WriteLine(" [x] Requesting 10 + 1");
+                var response = client.Call("10");
+                Console.WriteLine(" [.] Got '{0}'", response);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    class RPCClient : IDisposable
+    {
+        private IConnection connection;
+        private IModel channel;
+        private string replyQueueName;
+        private QueueingBasicConsumer consumer;
+
+        public RPCClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queue: replyQueueName,
+                noAck: true,
+                consumer: consumer);
+        }
+
+        public string Call(string message)
+        {
+            var corrId = Guid.NewGuid().ToString();
+            var props = channel.CreateBasicProperties();
+            props.ReplyTo = replyQueueName;
+            props.CorrelationId = corrId;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                routingKey: "rpc_queue",
+                basicProperties: props,
+                body: messageBytes);
+
+            while (true)
+            {
+                var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                if (ea.BasicProperties.CorrelationId == corrId)
+                    return Encoding.UTF8.GetString(ea.Body);
+            }
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ RabbitMqDemo/RPC/Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rpc_queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                channel.BasicQos(0, 1, false);
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(queue: "rpc_queue",
+                    noAck: false,
+                    consumer: consumer);
+                Console.WriteLine(" [x] Waiting RPC request.");
+
+                while (true)
+                {
+                    string response = null;
+                    var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                    var body = ea.Body;
+                    var props = ea.BasicProperties;
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine(" [.] n + 1", message);
+                        response = (n + 1).ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(" [.] {0}", ex.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "",
+                            routingKey: props.ReplyTo,
+                            basicProperties: replyProps,
+                            body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                            multiple: false);
+                    }
+                }
+            }
+        }
+    }
+}
