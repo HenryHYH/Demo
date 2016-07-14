051a1b84fb1cac874cfd830ffbc1fcb2912999ac
HenryHYH <henry.hyh@gmail.com>
2016-07-14 14:38:38 +0800

    Finish RabbitMq publish/subscribe demo

---
+++ RabbitMqDemo/PubSub/EmitLog/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = Console.ReadLine();
+                while (!string.IsNullOrWhiteSpace(message))
+                {
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "logs",
+                        routingKey: "",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    message = Console.ReadLine();
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
