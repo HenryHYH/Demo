fbe63339782fa437c937bc23e793d0f10f08bfa3
HenryHYH <henry.hyh@gmail.com>
2016-07-15 13:38:30 +0800

    加入接口测试项目

---
+++ SelfHostDemo/TestApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test();
+        }
+
+        private static void Test()
+        {
+            var sw = new Stopwatch();
+            IList<double> times = new List<double>();
+
+            Parallel.For(0, 1000, new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 50
+            }, i =>
+            {
+                sw.Restart();
+
+                Console.WriteLine("i = {0}", i);
+
+                var hander = new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip };
+                using (var client = new HttpClient(hander))
+                {
+                    var response = client.GetAsync("http://localhost:8000/api/user");
+                    var str = response.Result.Content.ReadAsStringAsync();
+                    Console.WriteLine(str.Result);
+                }
+
+                sw.Stop();
+                times.Add(sw.Elapsed.TotalMilliseconds);
+            });
+
+            Console.WriteLine("Avg = " + times.Average());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SelfHostDemo/TestApp/TestApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Flurl;
+using Flurl.Http;
+
+namespace TestApp
+{
+    public class TestApi
+    {
+        [Fact]
+        public void TestUserGet()
+        {
+            var result = "http://localhost:8000"
+                            .AppendPathSegment("API")
+                            .AppendPathSegment("User")
+                            .GetJsonListAsync()
+                            .Result;
+
+            Assert.Equal(3, result.Count());
+        }
+    }
+}
