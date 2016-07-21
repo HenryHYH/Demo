5cfc2f0eb368f283f7fa1a087c5204c7f19a8069
HenryHYH <henry.hyh@gmail.com>
2016-07-21 11:16:32 +0800

    添加部分 Linq 转换

--- DapperDemo/ConsoleApp/MyOrm/QueryCommand.cs
+++ DapperDemo/ConsoleApp/MyOrm/QueryCommand.cs
@@ -31,7 +31,7 @@ namespace ConsoleApp.MyOrm
 
         #endregion
 
-        #region Methods
+        #region Condition methods
 
         public QueryCommand<T> Where(Expression<Func<T, bool>> func)
         {
@@ -74,15 +74,141 @@ namespace ConsoleApp.MyOrm
             return this;
         }
 
+        #endregion
+
+        #region Execute methods
+
         public IList<T> ToList()
         {
-            var sql = new StringBuilder();
-            sql.AppendFormat("SELECT * FROM [{0}] [t] WITH(NOLOCK)", typeof(T).Name);
-            sql.AppendLine();
+            var command = GetSelectCommand();
+            var sql = command.Item1;
+            var parameters = command.Item2;
 
+            var list = new List<T>();
+            using (IDbConnection conn = new SqlConnection(CONNECTION_STRING))
+            {
+                var param = new DynamicParameters(parameters);
+                list = conn.Query<T>(sql, param).ToList();
+            }
+
+            return list;
+        }
+
+        public bool Any()
+        {
+            var command = GetSelectCommand(withoutOrderBy: true);
+            var sql = command.Item1;
+            var parameters = command.Item2;
+
+            sql = string.Format("SELECT TOP 1 1 FROM ({0}{1}) [t_any]", Environment.NewLine, sql);
+            var param = new DynamicParameters(parameters);
+
+            var result = false;
+            using (IDbConnection conn = new SqlConnection(CONNECTION_STRING))
+                result = conn.Query<bool>(sql, param).FirstOrDefault();
+
+            return result;
+        }
+
+        public int Count(Expression<Func<T, object>> func)
+        {
+            return Aggr<int>(func, "COUNT", isCastType: false);
+        }
+
+        public int CountDistinct(Expression<Func<T, object>> func)
+        {
+            return Aggr<int>(func, "COUNT", isCastType: false, isDistinct: true);
+        }
+
+        public TResult Sum<TResult>(Expression<Func<T, object>> func)
+        {
+            return Aggr<TResult>(func, "SUM");
+        }
+
+        public TResult Max<TResult>(Expression<Func<T, object>> func)
+        {
+            return Aggr<TResult>(func, "MAX");
+        }
+
+        public TResult Min<TResult>(Expression<Func<T, object>> func)
+        {
+            return Aggr<TResult>(func, "MIN");
+        }
+
+        public TResult Avg<TResult>(Expression<Func<T, object>> func)
+        {
+            return Aggr<TResult>(func, "AVG");
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private TResult Aggr<TResult>(Expression<Func<T, object>> func, string aggrType, bool isCastType = true, bool isDistinct = false)
+        {
+            var command = GetAggrCommand<TResult>(func, aggrType, isCastType, isDistinct);
+            var sql = command.Item1;
+            var parameters = command.Item2;
+
+            TResult data = default(TResult);
+            using (IDbConnection conn = new SqlConnection(CONNECTION_STRING))
+            {
+                var param = new DynamicParameters(parameters);
+                data = conn.QueryFirst<TResult>(sql, param);
+            }
+
+            return data;
+        }
+
+        private string CastType(Type type)
+        {
+            string result = "";
+
+            if (typeof(float) == type ||
+                typeof(double) == type ||
+                typeof(decimal) == type)
+                result = "decimal";
+            else if (typeof(short) == type ||
+                    typeof(int) == type ||
+                    typeof(long) == type)
+                result = "int";
+
+            return result;
+        }
+
+        private Tuple<string, IDictionary<string, object>> GetAggrCommand<TResult>(Expression<Func<T, object>> func, string aggrType, bool isCastType, bool isDistinct)
+        {
+            var analysisResult = new ExpressionAnalyzer(func).GetAnalysisResult();
+            var field = string.Join("", analysisResult.StackList).Replace("[t]", "[t_aggr]");
+
+            var typeName = typeof(TResult).Name;
+
+            var command = GetSelectCommand(true);
+            var sql = command.Item1;
+            var parameters = command.Item2;
+            var castType = CastType(typeof(TResult));
+
+            if (isCastType)
+                field = string.Format("CAST({0} AS {1})", field, castType);
+            if (isDistinct)
+                field = string.Format("DISTINCT {0}", field);
+
+            sql = string.Format("SELECT {0}({1}) FROM (\r\n{2}) [t_aggr]", aggrType, field, sql);
+
+            return new Tuple<string, IDictionary<string, object>>(sql, parameters);
+        }
+
+        public Tuple<string, IDictionary<string, object>> GetSelectCommand(bool withoutOrderBy = false)
+        {
+            var sqlBuilder = new StringBuilder();
             IDictionary<string, object> parameters = null;
-            IList<T> list = new List<T>();
 
+            // SELECT FROM
+            var tableName = typeof(T).Name;
+            var selectFrom = string.Format("SELECT * FROM [{0}] [t] WITH(NOLOCK)", tableName);
+            sqlBuilder.AppendLine(selectFrom);
+
+            // WHERE
             if (whereConds.Any())
             {
                 var exp = whereConds[0];
@@ -91,22 +217,37 @@ namespace ConsoleApp.MyOrm
 
                 var result = new ExpressionAnalyzer(exp).GetAnalysisResult();
                 var str = string.Join(" ", result.StackList);
-                sql.AppendLine("WHERE " + str);
+                sqlBuilder.AppendLine("WHERE " + str);
                 parameters = result.ParamList;
             }
+
+            // ORDER BY
+            var orderBy = "ORDER BY [t].[Id]";
             if (orderByConds.Any())
             {
                 var str = string.Join(", ", orderByConds.Select(x => string.Format("{0} {1}", x.Key, x.Value)));
-                sql.AppendLine("ORDER BY " + str);
+                orderBy = "ORDER BY " + str;
             }
 
-            using (IDbConnection conn = new SqlConnection(CONNECTION_STRING))
+            // PAGER
+            if (take.HasValue || skip.HasValue)
             {
-                var param = new DynamicParameters(parameters);
-                list = conn.Query<T>(sql.ToString(), param).ToList();
-            }
+                if (take.HasValue)
+                    sqlBuilder.Insert(0, string.Format("SELECT TOP {0} * FROM (", take.Value) + Environment.NewLine);
+                else
+                    sqlBuilder.Insert(0, "SELECT * FROM (" + Environment.NewLine);
+                sqlBuilder.AppendLine(") [t_row]");
 
-            return list;
+                if (skip.HasValue)
+                {
+                    sqlBuilder.Replace(selectFrom, string.Format("SELECT *, ROW_NUMBER() OVER ({0}) AS ROW_NUM FROM [{1}] [t] WITH(NOLOCK)", orderBy, tableName));
+                    sqlBuilder.AppendLine(string.Format("WHERE [t_row].[ROW_NUM] > {0}", skip.Value));
+                }
+            }
+            else if (!withoutOrderBy)
+                sqlBuilder.AppendLine(orderBy);
+
+            return new Tuple<string, IDictionary<string, object>>(sqlBuilder.ToString(), parameters);
         }
 
         #endregion
--- DapperDemo/ConsoleApp/TestMyOrm.cs
+++ DapperDemo/ConsoleApp/TestMyOrm.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 using ConsoleApp.Model;
 using ConsoleApp.MyOrm;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ConsoleApp
 {
-    public class TestMyOrm : BaseTest
+    public class TestMyOrm
     {
         #region Fields
 
         private readonly QueryCommand<User> command;
+        private readonly ITestOutputHelper output;
 
         #endregion
 
         #region Ctor
 
-        public TestMyOrm()
+        public TestMyOrm(ITestOutputHelper output)
         {
+            this.output = output;
             this.command = new QueryCommand<User>();
         }
 
@@ -44,6 +47,58 @@ namespace ConsoleApp
 
             Assert.Equal(3, list.Count);
             Assert.Equal(4, list[0].Age);
+        }
+
+        [Fact]
+        public void TestTake()
+        {
+            var list = command.Take(2)
+                              .ToList();
+
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void TestSkip()
+        {
+            var list = command.Skip(1)
+                              .ToList();
+
+            Assert.Equal(3, list.Count);
+            Assert.Equal(2, list[0].Age);
+        }
+
+        [Fact]
+        public void TestTakeSkip()
+        {
+            var list = command.OrderByDescding(x => x.Age)
+                              .Skip(1)
+                              .Take(2)
+                              .ToList();
+            output.WriteLine(command.GetSelectCommand().Item1);
+
+            Assert.Equal(2, list.Count);
+            Assert.Equal(3, list[0].Age);
+        }
+
+        [Fact]
+        public void TestAggr()
+        {
+            Assert.Equal(4, command.Count(x => x.Name));
+            Assert.Equal(1, command.CountDistinct(x => x.Name));
+            Assert.Equal(10, command.Sum<int>(x => x.Age));
+            Assert.Equal(4, command.Max<int>(x => x.Age));
+            Assert.Equal(1, command.Min<int>(x => x.Age));
+            Assert.Equal(2.5, command.Avg<double>(x => x.Age));
+        }
+
+        [Fact]
+        public void TestAny()
+        {
+            Assert.True(command.Any());
+
+            var any = command.Where(x => x.Id < 0).Any();
+            Assert.False(any);
         }
     }
 }
