db3873c1b556e47785d988f72ca5f1c5ebe56f3f
HenryHYH <henry.hyh@gmail.com>
2016-11-03 17:08:20 +0800

    SqlSugar Select Hello world

---
+++ SqlSugarDemo/ConsoleApp/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace ConsoleApp
+{
+    public class DbHelper
+    {
+        public static readonly string ConnectionString = "Data Source=192.168.111.20;Initial Catalog=Test;User ID=sa;Password=HWu7f8nH;";
+
+        public static SqlSugarClient GetInstance()
+        {
+            var db = new SqlSugarClient(ConnectionString);
+            db.IsEnableLogEvent = true;
+            db.LogEventStarting = (sql, par) => { Console.WriteLine(sql + " " + par + "\r\n"); };
+
+            return db;
+        }
+    }
+}
--- SqlSugarDemo/ConsoleApp/Program.cs
+++ SqlSugarDemo/ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp.Domains;
+using SqlSugar;
 
 namespace ConsoleApp
 {
@@ -10,6 +12,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var db = DbHelper.GetInstance())
+            {
+                var query = db.Queryable<User>()
+                              .Where(x => x.Enabled == true)
+                              .Where(x => x.Age > 3);
+
+                var sqlInfo = query.ToSql();
+                Console.WriteLine("SQL = " + sqlInfo.Key);
+                for (int i = 0, iMax = sqlInfo.Value.Length; i < iMax; i++)
+                    Console.WriteLine("P" + i + " = " + sqlInfo.Value[i]);
+
+                var list = query.ToList();
+                Console.WriteLine("Count = " + list.Count);
+            }
+
+            Console.ReadKey();
         }
     }
 }
