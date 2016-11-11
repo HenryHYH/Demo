20b9aa5d859ae59055106b607842d5e40c975f09
HenryHYH <henry.hyh@gmail.com>
2016-11-11 11:20:35 +0800

    添加测试

---
+++ DapperLinqDemo/ConsoleApp/Tests/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using Dapper.Extensions.Linq.CastleWindsor;
+using Dapper.Extensions.Linq.Core.Configuration;
+using Dapper.Extensions.Linq.Mapper;
+using Dapper.Extensions.Linq.Sql;
+
+namespace ConsoleApp.Tests
+{
+    public abstract class BaseTest
+    {
+        public static readonly WindsorContainer CONTAINER = new WindsorContainer();
+
+        public BaseTest()
+        {
+            DapperConfiguration
+                .Use()
+                .UseClassMapper(typeof(AutoClassMapper<>))
+                .UseContainer<ContainerForWindsor>(cfg => cfg.UseExisting(CONTAINER))
+                .UseSqlDialect(new SqlServerDialect())
+                .WithDefaultConnectionStringNamed("__DefaultSqlServer")
+                .FromAssembly("ConsoleApp.Domains")
+                .FromAssembly("ConsoleApp.Maps")
+                .Build();
+        }
+    }
+}
---
+++ DapperLinqDemo/ConsoleApp/Tests/Get.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Domains;
+using Dapper.Extensions.Linq.Core.Repositories;
+using Xunit;
+
+namespace ConsoleApp.Tests
+{
+    public class Get : BaseTest
+    {
+        private readonly IRepository<User> repository;
+
+        public Get()
+        {
+            this.repository = CONTAINER.Resolve<IRepository<User>>();
+        }
+
+        [Fact]
+        public void Helloworld()
+        {
+            var user = repository.Get(5);
+            Assert.Equal(5, user.Id);
+            Assert.Equal(100, user.Age);
+            Assert.Equal("Test", user.Name);
+            Assert.True(user.Enabled);
+        }
+
+        [Fact]
+        public void Query()
+        {
+            Expression<Func<User, bool>> predicate = x => x.Enabled && x.Age > 1;
+
+            var query = repository.Query(predicate);
+            var list = query.ToList();
+
+            Assert.Equal(3, list.Count);
+        }
+
+        [Fact]
+        public void Max()
+        {
+            var max = int.Parse(repository.QueryScalar("SELECT MAX(Age) FROM [User] WITH(NOLOCK)").ToString());
+
+            Assert.Equal(100, max);
+        }
+    }
+}
