c2c4e6ec190e131bd7b6862789588fe201822c55
Henry <henry.hyh@gmail.com>
2017-03-03 09:20:46 +0800

    添加 Entity Framework Model First Demo

---
+++ EntityFrameworkDemo/ConsoleApp/TestDal.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class TestDal
+    {
+        public int GetCount()
+        {
+            using (var context = new ATSEntities())
+            {
+                return context.T_Binding.Count();
+            }
+        }
+    }
+}
