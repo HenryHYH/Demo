f25f3131f58572ef640fac2b6d3e9ba56e349a25
Henry <henry.hyh@gmail.com>
2017-03-06 09:33:43 +0800

    Init MS Unity Demo

---
+++ UnityDemo/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 注册
+            IUnityContainer container = new UnityContainer();
+            //RegisterByCode(ref container);
+            RegisterByConfig(ref container);
+
+            var logger = container.Resolve<ILog>();
+            logger.Log("Hello world");
+
+            Console.ReadKey();
+        }
+
+        private static void RegisterByConfig(ref IUnityContainer container)
+        {
+            container.LoadConfiguration();
+        }
+
+        private static void RegisterByCode(ref IUnityContainer container)
+        {
+            container.RegisterType<ILog, ConsoleLog>();
+        }
+    }
+}
