31cc2453fed590b22610f66ec3003cd4487c6e3b
Henry <henry.hyh@gmail.com>
2017-03-10 11:16:28 +0800

    加入 CrystalQuartz 对 Quartz 进行监控

--- Topshelf/Helloworld/QuartzService.cs
+++ Topshelf/Helloworld/QuartzService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Quartz;
 using Helloworld.Jobs;
 using Quartz.Impl;
+using System.Collections.Specialized;
 
 namespace Helloworld
 {
@@ -17,18 +18,38 @@ namespace Helloworld
 
         public QuartzService()
         {
-            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            var properties = new NameValueCollection();
+            properties["quartz.scheduler.instanceName"] = "RemoteServerSchedulerClient";
+
+            // 设置线程池
+            properties["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz";
+            properties["quartz.threadPool.threadCount"] = "5";
+            properties["quartz.threadPool.threadPriority"] = "Normal";
+
+            // 远程输出配置
+            properties["quartz.scheduler.exporter.type"] = "Quartz.Simpl.RemotingSchedulerExporter, Quartz";
+            properties["quartz.scheduler.exporter.port"] = "556";
+            properties["quartz.scheduler.exporter.bindName"] = "QuartzScheduler";
+            properties["quartz.scheduler.exporter.channelType"] = "tcp";
+
+            var schedulerFactory = new StdSchedulerFactory(properties);
+            scheduler = schedulerFactory.GetScheduler();
         }
 
         private void Run()
         {
-            IJobDetail job = JobBuilder.Create<ShowTimeJob>().Build();
+            IJobDetail job = JobBuilder.Create<ShowTimeJob>()
+                                       .WithIdentity("MyJob", "Group1")
+                                       .Build();
+
             ITrigger trigger = TriggerBuilder.Create()
-                                        // .StartAt(DateTime.Now.AddSeconds(5))
-                                        .WithSimpleSchedule(x => x.WithIntervalInSeconds(5)
-                                                                  .RepeatForever()
-                                                                  //.WithMisfireHandlingInstructionIgnoreMisfires()
-                                                            )
+                                        .WithIdentity("MyJobTrigger", "GroupA")
+                                        //.StartAt(DateTime.Now.AddSeconds(5))
+                                        //.WithSimpleSchedule(x => x.WithIntervalInSeconds(5)
+                                        //                          .RepeatForever()
+                                        //                    )
+                                        .WithCronSchedule("0/3 * * ? * *")
+                                        .StartNow()
                                         .Build();
 
             scheduler.ScheduleJob(job, trigger);
---
+++ Topshelf/MonitorQuartzWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MonitorQuartzWebApp.MvcApplication" Language="C#" %>
