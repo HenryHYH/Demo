e688010c72d0defff196834d701728e6910bff84
Henry <henry.hyh@gmail.com>
2017-03-14 11:12:15 +0800

    添加 xUnit Demo

---
+++ xUnitDemo/SDK.Tests/HelloworldTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDK;
+using Xunit;
+
+namespace SDK.Tests
+{
+    public class HelloworldTest
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-1, 1, 0)]
+        public void TestAdd(int a, int b, int expected)
+        {
+            var helloworld = new Helloworld();
+            var result = helloworld.Add(a, b);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
