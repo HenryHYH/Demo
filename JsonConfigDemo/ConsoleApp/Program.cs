048693738d36956df2760cef1cd06718fc7750d0
Henry <henry.hyh@gmail.com>
2017-03-22 11:25:14 +0800

    添加 JsonConfig Demo

---
+++ JsonConfigDemo/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonConfig;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // default.conf 属性 - 生成操作 - 嵌入的资源
+
+            var storeOwner = Config.Default.StoreOwner;
+
+            Console.WriteLine("StoreOwner = " + storeOwner);
+
+            foreach (var fruit in Config.Default.Fruits)
+                Console.WriteLine(fruit);
+
+            Console.ReadKey();
+        }
+    }
+}
