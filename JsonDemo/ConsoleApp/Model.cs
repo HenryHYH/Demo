6696abe1ab1c90008299b0d77bb9a35c0faac4b0
Henry <henry.hyh@gmail.com>
2017-04-13 14:49:00 +0800

    添加 Json.Net 测试，添加字段排序的测试

---
+++ JsonDemo/ConsoleApp/Model.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Model
+    {
+        [JsonProperty(PropertyName = "a", Order = 2)]
+        public string A { get; set; }
+
+        [JsonProperty(PropertyName = "b", Order = 1)]
+        public string B { get; set; }
+
+        //[JsonProperty(PropertyName = "c", Order = 3)]
+        public int C { get; set; }
+
+        [JsonProperty(Order = int.MaxValue)]
+        public string Sign { get; set; }
+    }
+}
---
+++ JsonDemo/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new Model() { A = "AA", B = "BB", C = 1 };
+            var json = JsonConvert.SerializeObject(m);
+
+            var dt = JsonConvert.DeserializeObject<IDictionary<string, string>>(json);
+            var str = string.Join("", dt.Values);
+
+            Console.WriteLine(json);
+            Console.WriteLine(str);
+
+            Console.WriteLine(Enum.Test.ToString("D"));
+
+            Console.ReadKey();
+        }
+    }
+}
