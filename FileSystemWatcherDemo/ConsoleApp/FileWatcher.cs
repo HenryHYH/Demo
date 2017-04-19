9c7c34ca6f91bf4189a74ee0a681c11f48a12e39
Henry <henry.hyh@gmail.com>
2017-04-19 17:02:14 +0800

    添加 FileSystemWatcher Demo

---
+++ FileSystemWatcherDemo/ConsoleApp/FileWatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public class FileWatcher
+    {
+        private readonly FileSystemWatcher watcher;
+
+        public FileWatcher(string path, string filter)
+        {
+            watcher = new FileSystemWatcher(path, filter);
+            watcher.Changed += Watcher_Changed;
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                // 修复执行多次的问题
+                watcher.EnableRaisingEvents = false;
+
+                Console.WriteLine("{0}_{1}_{2}",
+                                  e.FullPath,
+                                  e.Name,
+                                  e.ChangeType.ToString());
+            }
+            finally
+            {
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+    }
+}
---
+++ FileSystemWatcherDemo/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WatcherStrat(@"D:\", "*.*");
+            var watcher = new FileWatcher(@"D:\", "*.*");
+
+            Console.ReadKey();
+        }
+    }
+}
