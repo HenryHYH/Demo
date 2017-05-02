067bff0078ee8de9e09969f79f25d064efa4970c
Henry <henry.hyh@gmail.com>
2017-05-02 11:02:27 +0800

    xUnit 添加在output窗口数据的代码

---
+++ xUnitDemo/SDK.Tests/WriteLineTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace SDK.Tests
+{
+    public class WriteLineTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public WriteLineTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            output.WriteLine("Hello world");
+
+            Assert.Equal(1, 1);
+        }
+    }
+}
