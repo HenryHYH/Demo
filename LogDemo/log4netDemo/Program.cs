380a39747c6912e7017b89834acdd861b0eae630
Henry <henry.hyh@gmail.com>
2017-06-07 10:15:19 +0800

    LogDemo，添加log4net示例

---
+++ LogDemo/log4netDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+
+namespace log4netDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog Log = LogManager.GetLogger("SomeName");
+            ILog SummaryLog = LogManager.GetLogger("Summary");
+            Log.Debug("Processing");
+            SummaryLog.Debug("Processing2");
+
+            Console.WriteLine("Press any keys");
+            Console.ReadKey();
+        }
+    }
+}
