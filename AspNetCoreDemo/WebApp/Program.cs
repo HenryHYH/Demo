da5c086956afcfa8d8e60399ebc27550d832f056
Henry <henry.hyh@gmail.com>
2017-08-09 16:28:31 +0800

    AspNetCoreDemo，添加命令行参数用于启动时改变参数

--- AspNetCoreDemo/WebApp/Program.cs
+++ AspNetCoreDemo/WebApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace WebApp
@@ -7,12 +8,18 @@ namespace WebApp
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
+                //.UseUrls("http://*:5000", "http://*:5001")
                 .Build();
 
             host.Run();
