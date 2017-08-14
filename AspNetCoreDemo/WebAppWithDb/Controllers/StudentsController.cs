a2eff3ac1486a153e9df99be99fb4d2d73a6ad03
Henry <henry.hyh@gmail.com>
2017-08-14 09:36:54 +0800

    AspNetCoreDemo，完成EF Core示例

---
+++ AspNetCoreDemo/WebAppWithDb/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebAppWithDb.Data;
+
+namespace WebAppWithDb.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext context;
+
+        public StudentsController(SchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Students.ToListAsync());
+        }
+    }
+}
--- AspNetCoreDemo/WebAppWithDb/Startup.cs
+++ AspNetCoreDemo/WebAppWithDb/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebAppWithDb.Data;
 
 namespace WebAppWithDb
 {
@@ -27,12 +25,21 @@ namespace WebAppWithDb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            SchoolContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -55,6 +62,8 @@ namespace WebAppWithDb
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Initialize(context);
         }
     }
 }
