fec65aad70aed85bcda97bb2d336cb3a4d34e52d
Henry <henry.hyh@gmail.com>
2017-08-14 10:36:01 +0800

    AspNetCoreDemo，添加通过TestHost来测试Controller的示例

---
+++ AspNetCoreDemo/WebApp.Tests/BookControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApp.Tests
+{
+    public class BookControllerTest
+    {
+        private readonly TestServer server;
+        private readonly HttpClient client;
+
+        public BookControllerTest()
+        {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
+        }
+
+        [Fact]
+        public async Task TestIndexAsync()
+        {
+            var response = await client.GetAsync("/book");
+            response.EnsureSuccessStatusCode();
+
+            var message = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("1", message);
+        }
+    }
+}
