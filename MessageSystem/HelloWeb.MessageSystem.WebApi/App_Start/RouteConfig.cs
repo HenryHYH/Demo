1fa8d5101540a7b5456c6544d921b2ded6acae0b
Henry <henry.hyh@gmail.com>
2017-08-29 16:25:52 +0800

    MessageSystem，添加日志显示

--- MessageSystem/HelloWeb.MessageSystem.WebApi/App_Start/RouteConfig.cs
+++ MessageSystem/HelloWeb.MessageSystem.WebApi/App_Start/RouteConfig.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace HelloWeb.MessageSystem.WebApi
 {
+    /// <summary>
+    /// 路由配置
+    /// </summary>
     public class RouteConfig
     {
+        /// <summary>
+        /// 注册路由
+        /// </summary>
+        /// <param name="routes">路由集合</param>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
