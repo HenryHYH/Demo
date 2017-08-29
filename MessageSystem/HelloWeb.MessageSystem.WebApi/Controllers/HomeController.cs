5ab0cd57c0497ab64284280441883e6976f076a3
Henry <henry.hyh@gmail.com>
2017-08-29 15:00:07 +0800

    MessageSystem，Init webapi

---
+++ MessageSystem/HelloWeb.MessageSystem.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace HelloWeb.MessageSystem.WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content(string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.Now));
+        }
+    }
+}
---
+++ MessageSystem/HelloWeb.MessageSystem.WebApi/Controllers/LogController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace HelloWeb.MessageSystem.WebApi.Controllers
+{
+    public class LogController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "A", "B", "C" };
+        }
+    }
+}
