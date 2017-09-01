b0aad85e8d846eadb1e770f59114eae53cf3be6a
Henry <henry.hyh@gmail.com>
2017-09-01 17:09:08 +0800

    MessageSystem，添加SDK和测试的Client

---
+++ MessageSystem/HelloWeb.MessageSystem.Client/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HelloWeb.MessageSystem.SDK;
+using HelloWeb.MessageSystem.SDK.Models;
+using System;
+using System.Web.Mvc;
+
+namespace HelloWeb.MessageSystem.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            IHelloWebMessageSystem client = new HelloWebMessageSystem(new Uri("http://localhost:57878/"), new AnonymousCredential());
+            var response = client.Log.PostByrequest(new LogModel()
+            {
+                ProjectName = "HelloWeb",
+                AppName = "Client",
+                Level = 10,
+                Message = "Hello world",
+                UniqueSequence = Guid.NewGuid().ToString(),
+                CTime = DateTime.Now,
+                Exception = new Exception("Hello Exception")
+            });
+
+            return Content(string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.Now));
+        }
+    }
+}
