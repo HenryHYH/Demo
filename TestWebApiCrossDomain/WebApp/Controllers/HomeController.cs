2b6f0a671936df2cd53bd61941f0df308ae06418
Henry <henry.hyh@gmail.com>
2018-04-22 15:32:57 +0800

    添加接口允许多个Access-Control-Allow-Origin的示例代码

---
+++ TestWebApiCrossDomain/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.Mvc;
+using WebApp.Infrastructure;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ApiPermissionFilter]
+        public ActionResult Post()
+        {
+            var url = Request.UrlReferrer.GetLeftPart(UriPartial.Authority);
+
+            //Response.Headers.Remove("Access-Control-Allow-Origin");
+            //Response.AddHeader("Access-Control-Allow-Origin", url);
+
+            //Response.Headers.Remove("Access-Control-Allow-Credentials");
+            //Response.AddHeader("Access-Control-Allow-Credentials", "true");
+
+            //Response.Headers.Remove("Access-Control-Allow-Methods");
+            //Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+            return Json(new
+            {
+                Success = true,
+                ResponseTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"),
+                Url = url
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TestWebApiCrossDomain/WebApp/Infrastructure/ApiPermissionFilterAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace WebApp.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ApiPermissionFilterAttribute : ActionFilterAttribute
+    {
+        private static readonly string HOST_PATTERN = "([^.]+\\.[^.]{1,3}(\\.[^.]{1,3})?)$";
+
+        private string GetDomain(string host)
+        {
+            var match = Regex.Match(host, HOST_PATTERN);
+
+            return match.Success ? match.Value : host;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var host = filterContext.HttpContext.Request.UrlReferrer.Host;
+            var domain = GetDomain(host);
+
+            if (!string.Equals("henry.com", domain, StringComparison.InvariantCultureIgnoreCase))
+            {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var url = filterContext.HttpContext.Request.UrlReferrer.GetLeftPart(UriPartial.Authority);
+
+            filterContext.HttpContext.Response.Headers.Remove("Access-Control-Allow-Origin");
+            filterContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", url);
+
+            filterContext.HttpContext.Response.Headers.Remove("Access-Control-Allow-Credentials");
+            filterContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+
+            filterContext.HttpContext.Response.Headers.Remove("Access-Control-Allow-Methods");
+            filterContext.HttpContext.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
