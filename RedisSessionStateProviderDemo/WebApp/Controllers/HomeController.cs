7a66bf4f9a29856d64942cd47aea268e85cf72f6
Henry <henry.hyh@gmail.com>
2018-07-22 00:53:57 +0800

    RedisSessionStateProviderDemo，添加Demo

---
+++ RedisSessionStateProviderDemo/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session.Timeout = 10;
+            var val = Session["Helloworld"];
+            if (null == val)
+            {
+                Session.Add("Helloworld", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.DateTimeFormatInfo.InvariantInfo));
+                ViewBag.Value = "NULL";
+            }
+            else
+                ViewBag.Value = val.ToString();
+
+            Session["ABC"] = DateTime.Now.ToString();
+
+            return View();
+        }
+    }
+}
