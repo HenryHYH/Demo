5b83d05ccea5d443ea6d890b72850bfd9773a540
Henry <henry.hyh@gmail.com>
2018-08-24 17:09:48 +0800

    GatewayDemo，Init code

---
+++ GatewayDemo/WebApp1/Controllers/CounterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        private static int count = 0;
+
+        [HttpGet]
+        public string Count()
+        {
+            return $"Count {++count} from WebApi1";
+        }
+    }
+}
