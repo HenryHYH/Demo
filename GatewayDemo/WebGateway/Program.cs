767609ceae00990797a01179967f6a818278c792
Henry <henry.hyh@gmail.com>
2018-08-27 15:54:43 +0800

    GatewayDemo，加入Ocelot的配置

--- GatewayDemo/WebGateway/Program.cs
+++ GatewayDemo/WebGateway/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace WebGateway
 {
@@ -19,6 +13,12 @@ namespace WebGateway
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                {
+                    builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("ocelot.json");
+                })
+                .UseStartup<Startup>()
+                .UseUrls("http://*:0");
     }
 }
--- GatewayDemo/WebGateway/Startup.cs
+++ GatewayDemo/WebGateway/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace WebGateway
 {
@@ -24,6 +21,8 @@ namespace WebGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot()
+                    .AddConsul();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -35,6 +34,7 @@ namespace WebGateway
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseOcelot().Wait();
             app.UseMvc();
         }
     }
