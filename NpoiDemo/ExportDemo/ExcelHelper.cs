e12efa2a033aeff74d3fb0577117082de77d918b
Henry <henry.hyh@gmail.com>
2018-10-29 08:16:56 +0800

    NpoiDemo，Init demo

---
+++ NpoiDemo/ExportDemo/ExcelHelper.cs
@@ -0,0 +1,25 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+
+namespace ExportDemo
+{
+    public static class ExcelHelper
+    {
+        public static void Export(string file = @"helloworld.xlsx")
+        {
+            using (var fs = new FileStream(file, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+
+                var sheet = workbook.CreateSheet("SheetA");
+
+                var row = sheet.CreateRow(0);
+                var cell = row.CreateCell(0);
+                cell.SetCellValue("Hello world");
+
+                workbook.Write(fs);
+            }
+        }
+    }
+}
---
+++ NpoiDemo/ExportDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExportDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            ExcelHelper.Export();
+            Console.WriteLine("Finish");
+            Console.ReadLine();
+        }
+    }
+}
