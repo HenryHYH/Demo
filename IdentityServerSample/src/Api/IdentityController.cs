a037fae846a98572cba8dc4202b488c7c6028aa5
Henry <henry.hyh@gmail.com>
2019-01-22 16:05:04 +0800

    IdentityServerSample，QuickStarts1，protecting an API using Client Credentials

---
+++ IdentityServerSample/src/Api/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        public virtual IActionResult Get()
+        {
+            return new JsonResult(User.Claims.Select(x => new { x.Type, x.Value }));
+        }
+    }
+}
---
+++ IdentityServerSample/src/Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            MainImpl().GetAwaiter().GetResult();
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+
+        private static async Task MainImpl()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenRes = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            if (tokenRes.IsError)
+            {
+                Console.WriteLine(tokenRes.Error);
+                return;
+            }
+            Console.WriteLine(tokenRes.Json);
+
+            client = new HttpClient();
+            client.SetBearerToken(tokenRes.AccessToken);
+
+            var res = await client.GetAsync("http://localhost:5001/identity");
+            if (!res.IsSuccessStatusCode)
+                Console.WriteLine(res.StatusCode);
+            else
+            {
+                var content = await res.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
