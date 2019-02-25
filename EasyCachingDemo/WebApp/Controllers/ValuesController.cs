5fc458e92a623f022368b69699592c8b6e3def25
Henry <henry.hyh@gmail.com>
2019-02-25 10:04:54 +0800

    EasyCachingDemo，Init InMemory Demo

---
+++ EasyCachingDemo/WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using EasyCaching.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IEasyCachingProvider cachingProvider;
+
+        public ValuesController(IEasyCachingProvider cachingProvider)
+        {
+            this.cachingProvider = cachingProvider;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var value = string.Empty;
+            var cachedValue = cachingProvider.Get<string>("TestKey");
+            if (cachedValue.HasValue)
+                value = cachedValue.Value;
+            else
+            {
+                value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff");
+                cachingProvider.Set("TestKey", value, TimeSpan.FromSeconds(3));
+            }
+
+            return new string[] { "GET", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff"), value };
+        }
+    }
+}
