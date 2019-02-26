9f189e4b8a0426301a18147462a5d23cdf2b8c7b
Henry <henry.hyh@gmail.com>
2019-02-26 09:57:48 +0800

    PollyDemo，Init Retry demo

---
+++ PollyDemo/ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Polly;
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            try
+            {
+                Retry();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        private static void Retry()
+        {
+            var policy = Policy.Handle<Exception>()
+                            .WaitAndRetry(3, cnt => TimeSpan.FromSeconds(Math.Pow(2, cnt)), (err, countdown) =>
+                            {
+                                Console.WriteLine("Start retry");
+                                Console.WriteLine(err.Message);
+                                Console.WriteLine(countdown);
+                                Console.WriteLine("End retry");
+                            });
+
+            policy.Execute(() =>
+            {
+                Console.WriteLine("Before execute");
+
+                throw new Exception("Hello exeception");
+            });
+        }
+    }
+}
