bc53627613bdf3a2b45c17f2ecae8e4923270ffc
Henry <henry.hyh@gmail.com>
2019-02-27 14:17:56 +0800

    ScrutorDemo，Init Scrutor Demo

---
+++ ScrutorDemo/WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApp.Services;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public ValuesController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public ActionResult<string> Get()
+        {
+            var name = userService.GetName();
+
+            return $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff")}] - {name}";
+        }
+    }
+}
---
+++ ScrutorDemo/WebApp/Services/Services.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApp.Services
+{
+    public interface IUserService
+    {
+        string GetName();
+    }
+
+    public class UserService : IUserService
+    {
+        public string GetName()
+        {
+            return "Henry";
+        }
+    }
+
+    public interface IUserRepository { }
+
+    public class UserRepository : IUserRepository { }
+
+    public interface IProductRepository { }
+
+    public class ProductRepository : IProductRepository { }
+}
