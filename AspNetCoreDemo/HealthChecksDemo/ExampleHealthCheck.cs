e9060d9900da69f1624876f1f71b5ca30ed789da
Henry <henry.hyh@gmail.com>
2019-02-28 09:52:24 +0800

    AspNetCoreDemo，添加HealthCheck demo

---
+++ AspNetCoreDemo/HealthChecksDemo/ExampleHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecksDemo
+{
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var healthy = DateTime.Now.Second % 3 != 0;
+
+            if (healthy)
+                return Task.FromResult(HealthCheckResult.Healthy("Healthy"));
+
+            return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy"));
+        }
+    }
+}
