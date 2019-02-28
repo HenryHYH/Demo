5dc956554ebbcc6d65c70536c75427177fcdec74
Henry <henry.hyh@gmail.com>
2019-02-28 10:58:08 +0800

    AspNetCoreDemo，集成数据库的HealthCheck

--- AspNetCoreDemo/HealthChecksDemo/Startup.cs
+++ AspNetCoreDemo/HealthChecksDemo/Startup.cs
@@ -1,18 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace HealthChecksDemo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHealthChecks()
-                .AddCheck<ExampleHealthCheck>("example_health_check");
+                .AddCheck<ExampleHealthCheck>("example_health_check")
+                .AddSqlServer(Configuration.GetConnectionString("DefaultConnection"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,7 +35,24 @@ namespace HealthChecksDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHealthChecks("/hc");
+            app.UseHealthChecks("/hc", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+            {
+                ResponseWriter = (httpContext, result) =>
+                {
+                    httpContext.Response.ContentType = "application/json";
+
+                    var json = new JObject(
+                                new JProperty("status", result.Status.ToString()),
+                                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                                    new JProperty(pair.Key, new JObject(
+                                        new JProperty("status", pair.Value.Status.ToString()),
+                                        new JProperty("description", pair.Value.Description),
+                                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                                            p => new JProperty(p.Key, p.Value))))))))));
+
+                    return httpContext.Response.WriteAsync(json.ToString(Formatting.Indented));
+                }
+            });
 
             app.Run(async (context) =>
             {
