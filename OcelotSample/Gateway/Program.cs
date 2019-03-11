f1cae82d188c48fa7e6172eaaac2f5591579db86
Henry <henry.hyh@gmail.com>
2019-03-11 14:03:52 +0800

    OcelotSample，Init Ocelot Sample

---
+++ OcelotSample/Gateway/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("configuration.json")
+                        .AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+        }
+    }
+}
---
+++ OcelotSample/WebApi2/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "WebApi2", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff") };
+        }
+    }
+}
